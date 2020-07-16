3e593c2930cebbb2894b8f749e11e2d48b5ff6cc
Amirerfan <amirerfan.siamaky@gmail.com>
2020-07-16 14:53:30 +0430

    Room Page Chat UI Initial Commit

--- src/components/RoomPage/RoomPageChat.vue
+++ src/components/RoomPage/RoomPageChat.vue
@@ -1,9 +1,29 @@
 <template>
-  <div class="room-page">chat</div>
+  <div class="room-page-chat">
+    <SingleMessage />
+    <SingleMessage />
+    <SingleMessage />
+    <SingleMessage />
+    <SingleMessage />
+    <SingleMessage />
+    <SingleMessage />
+    <SingleMessage />
+    <SingleMessage />
+    <SingleMessage />
+  </div>
 </template>
 
 <script>
+import SingleMessage from "@/components/SingleMessage.vue";
+
 export default {
-  name: "RoomPageChat"
+  name: "RoomPageChat",
+  components: {
+    SingleMessage
+  },
+  mounted() {
+    let roomPageChat = document.getElementsByClassName("room-page-chat")[0];
+    roomPageChat.scrollTop = roomPageChat.scrollHeight;
+  }
 };
 </script>
---
+++ src/components/SingleMessage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="single-message single-message--sender-you">
+    <div
+      class="single-message__sender"
+    >{this.props.message.sender}{this.props.message.sender}{this.props.message.sender}</div>
+    <div class="single-message__text">{this.props.message.text}</div>
+    <div class="single-message__time">{this.props.message.time}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingleMessage"
+};
+</script>
