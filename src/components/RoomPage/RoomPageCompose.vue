3b796b9b5d01e2053aaedf91f79ed44b63f3e51d
Amirerfan <amirerfan.siamaky@gmail.com>
2020-07-16 14:17:19 +0430

    Room Page Compose UI Initial Commit

--- src/components/RoomPage/RoomPageCompose.vue
+++ src/components/RoomPage/RoomPageCompose.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="room-page-compose">compose</div>
+  <div class="room-page-compose">
+    <div class="room-page-compose__text">
+      <b-form-textarea v-model="text" debounce="500" rows="1" max-rows="3" placeholder="Compose"></b-form-textarea>
+    </div>
+    <div class="room-page-compose__buttons">
+      <b-icon icon="chevron-right" class="room-page-compose__buttons__send-button"></b-icon>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "RoomPageCompose"
+  name: "RoomPageCompose",
+  data() {
+    return {
+      text: ""
+    };
+  }
 };
 </script>
