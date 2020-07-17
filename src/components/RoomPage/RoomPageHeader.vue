bbd3bee3d0b7c056912c5f254b05588fab603c46
Amirerfan <amirerfan.siamaky@gmail.com>
2020-07-17 16:29:23 +0430

    Add Modal to Room Page

--- src/components/RoomPage/RoomPageHeader.vue
+++ src/components/RoomPage/RoomPageHeader.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="room-page-header">
+  <div @click="$bvModal.show('room-detail-modal')" class="room-page-header">
     <div class="room-page-header__room-name">Cafepay</div>
 
     <div class="room-page-header__video-call">
       <b-icon icon="camera-video-fill"></b-icon>
     </div>
+
+    <b-modal id="room-detail-modal" title="Cafepay" ok-only no-stacking>
+      <p class="my-2">First Modal</p>
+      <b-button @click="$bvModal.show('create-exam-modal')">+</b-button>
+    </b-modal>
+
+    <b-modal id="create-exam-modal" size="lg" title="Setup New Exam" ok-only no-stacking>
+      <p class="my-2">Label</p>
+      <b-button v-b-modal.modal-multi-3 size="sm">Set Exam</b-button>
+    </b-modal>
+
+    <b-modal id="modal-multi-3" size="sm" title="Third Modal" ok-only>
+      <p class="my-1">Third Modal</p>
+    </b-modal>
   </div>
 </template>
 
